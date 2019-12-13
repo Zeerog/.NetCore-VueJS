f9c0c254586d28f1227b73366aea852a5b5aa1ad
Zeerog <55512675+Zeerog@users.noreply.github.com>
2019-12-14 01:12:25 +0200

    Complete second task in C#

---
+++ C#/SecondTask.Classes/SecondTask.Classes/01.Address/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01.Address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.Index = "55105";
+            address.Country = "UA";
+            address.City = "Dnepr";
+            address.Street = "Shevcghenko";
+            address.House = "14";
+            address.Apartment = "D16";
+            Console.WriteLine("index = "+ address.Index);
+            Console.WriteLine("country = " + address.Country);
+            Console.WriteLine("city = " + address.City);
+            Console.WriteLine("street = " + address.Street);
+            Console.WriteLine("house = " + address.House);
+            Console.WriteLine("apartment = " + address.Apartment);
+            Console.ReadLine();
+        }
+    }
+    class Address
+    {
+        public string Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+    }
+}
