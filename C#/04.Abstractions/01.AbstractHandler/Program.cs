c131fa1ad8b9e31acf6b20a356c8c2b8d9ba9bfd
Zeerog <55512675+Zeerog@users.noreply.github.com>
2020-02-08 14:03:03 +0200

    Finish C# Abstractions task

---
+++ C#/04.Abstractions/01.AbstractHandler/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace _01.AbstractHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string formatDoc;
+            while (true)
+            {
+                Console.WriteLine("Enter document format(TXT/XML/DOC/QUIT): ");
+                formatDoc = Console.ReadLine();
+
+                if (formatDoc == "DOC")
+                {
+                    AbstractHandler doc = new DOCHandler();
+                    doc.open();
+                    doc.create();
+                    doc.change();
+                    doc.save();
+                }
+                else if (formatDoc == "XML")
+                {
+                    AbstractHandler xml = new XMLHandler();
+                    xml.open();
+                    xml.create();
+                    xml.change();
+                    xml.save();
+                }
+                else if (formatDoc == "TXT")
+                {
+                    AbstractHandler txt = new TXTHandler();
+                    txt.open();
+                    txt.create();
+                    txt.change();
+                    txt.save();
+                }
+                else if (formatDoc == "QUIT")
+                {
+                    break;
+                }
+                else
+                    Console.WriteLine("Failed format! Enter again!");
+            }
+
+        }
+    }
+    public abstract class AbstractHandler
+    {
+        public abstract void open();
+        public abstract void create();
+        public abstract void change();
+        public abstract void save();
+    }
+    public class XMLHandler : AbstractHandler
+    {
+        override public void open()
+        {
+            Console.WriteLine("Open XML");
+        }
+
+        override public void create()
+        {
+            Console.WriteLine("Create XML");
+        }
+
+        override public void change()
+        {
+            Console.WriteLine("Change XML");
+        }
+
+        override public void save()
+        {
+            Console.WriteLine("Save XML");
+        }
+    }
+    public class TXTHandler : AbstractHandler
+    {
+        override public void open()
+        {
+            Console.WriteLine("Open TXT");
+        }
+
+        override public void create()
+        {
+            Console.WriteLine("Create TXT");
+        }
+
+        override public void change()
+        {
+            Console.WriteLine("Change TXT");
+        }
+
+        override public void save()
+        {
+            Console.WriteLine("Save TXT");
+        }
+    }
+    public class DOCHandler : AbstractHandler
+    {
+        override public void open()
+        {
+            Console.WriteLine("Open DOC");
+        }
+
+        override public void create()
+        {
+            Console.WriteLine("Create DOC");
+        }
+
+        override public void change()
+        {
+            Console.WriteLine("Change DOC");
+        }
+
+        override public void save()
+        {
+            Console.WriteLine("Save DOC");
+        }
+    }
+}
---
+++ C#/04.Abstractions/02.Player/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _02.Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            ((IPlayable)player).Play();
+            ((IPlayable)player).Pause();
+            ((IPlayable)player).Stop();
+            ((IRecodable)player).Record();
+            ((IRecodable)player).Pause();
+            ((IRecodable)player).Stop();
+
+            //Или
+            //IPlayable playable = new Player();
+            //IRecodable recodable = new Player();
+            //playable.Play();
+            //playable.Pause();
+            //playable.Stop();
+            //recodable.Record();
+            //recodable.Pause();
+            //recodable.Stop();
+        }
+    }
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+    class Player : IPlayable, IRecodable
+    {
+        void IPlayable.Play()
+        {
+            Console.WriteLine("Play");
+        }
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Pause Player");
+        }
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Stop Player");
+        }
+        void IRecodable.Record()
+        {
+            Console.WriteLine("Record");
+        }
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Pause Record");
+        }
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Stop Record");
+        }
+    }
+}
